6adb9584e0c7720d712ae6dfb97dc9a8b53fd64e
srkimdev <emirated3858@gmail.com>
2024-08-15 17:55:35 +0900

    add tabbaricon in TabBarViewController

--- Here_come/Tab/TabBarViewController.swift
+++ Here_come/Tab/TabBarViewController.swift
@@ -12,17 +12,24 @@ final class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.tabBar.tintColor = .black
+        self.tabBar.barTintColor = .white
+        
         let first = AccomodationViewController()
         let nav1 = UINavigationController(rootViewController: first)
+        nav1.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "home.fill"))
         
         let second = RecommendViewController()
         let nav2 = UINavigationController(rootViewController: second)
+        nav2.tabBarItem = UITabBarItem(title: "소셜링", image: UIImage(systemName: "leaf"), selectedImage: UIImage(systemName: "leaf.fill"))
         
         let third = FreeBoardViewController()
         let nav3 = UINavigationController(rootViewController: third)
+        nav3.tabBarItem = UITabBarItem(title: "게시판", image: UIImage(systemName: "newspaper"), selectedImage: UIImage(systemName: "newspaper.fill"))
         
         let fourth = ProfileManageViewController()
         let nav4 = UINavigationController(rootViewController: fourth)
+        nav4.tabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
         
         setViewControllers([nav1, nav2, nav3, nav4], animated: true)
         
